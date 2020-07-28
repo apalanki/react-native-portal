5a89549cd3d547f8f2a3eaafdf0c54e33029caf8
Anudeep Palanki <anudeep.palanki@monsanto.com>
2020-07-28 14:36:02 -0500

    use common describe to run all the UTs

--- index.test.jsx
+++ index.test.jsx
@@ -1,66 +1,70 @@
 import React from 'react';
 
-import { configure, mount } from 'enzyme';
+import {configure, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { PortalProvider, WhitePortal, BlackPortal } from '.';
+import {BlackPortal, PortalProvider, WhitePortal} from '.';
 
-configure({ adapter: new Adapter() });
+configure({adapter: new Adapter()});
 const YourApp = () => <div>ok</div>;
-test('smoke test', () => {
-  const wrapper = mount(
-    <PortalProvider>
-      <YourApp />
-    </PortalProvider>
-  );
-  expect(wrapper.contains('ok')).toBeTruthy();
-  wrapper.unmount();
-});
 
-test('basic', () => {
-  const wrapper = mount(
-    <PortalProvider>
-      <div>
-        a
-        <WhitePortal name="one" />
-        b
-        <BlackPortal name="one">1</BlackPortal>
-        c
-      </div>
-    </PortalProvider>
-  );
-  expect(wrapper.text()).toBe('a1bc');
-  wrapper.unmount();
-});
+describe("all tests", () => {
+    test('smoke test', () => {
+        const wrapper = mount(
+            <PortalProvider>
+                <YourApp/>
+            </PortalProvider>
+        );
+        expect(wrapper.contains('ok')).toBeTruthy();
+        wrapper.unmount();
+    });
 
-test('name mismatch', () => {
-  const wrapper = mount(
-    <PortalProvider>
-      <div>
-        a
-        <WhitePortal name="two" />
-        b
-        <BlackPortal name="one">1</BlackPortal>
-        c
-      </div>
-    </PortalProvider>
-  );
-  expect(wrapper.text()).toBe('abc');
-  wrapper.unmount();
-});
+    test('basic', () => {
+        const wrapper = mount(
+            <PortalProvider>
+                <div>
+                    a
+                    <WhitePortal name="one"/>
+                    b
+                    <BlackPortal name="one">1</BlackPortal>
+                    c
+                </div>
+            </PortalProvider>
+        );
+        expect(wrapper.text()).toBe('a1bc');
+        wrapper.unmount();
+    });
 
-test('multiple WhitePortal', () => {
-  const wrapper = mount(
-    <PortalProvider>
-      <div>
-        a
-        <WhitePortal name="one" />
-        <WhitePortal name="one" />
-        b
-        <BlackPortal name="one">1</BlackPortal>
-        c
-      </div>
-    </PortalProvider>
-  );
-  expect(wrapper.text()).toBe('a11bc');
-  wrapper.unmount();
-});
+    test('name mismatch', () => {
+        const wrapper = mount(
+            <PortalProvider>
+                <div>
+                    a
+                    <WhitePortal name="two"/>
+                    b
+                    <BlackPortal name="one">1</BlackPortal>
+                    c
+                </div>
+            </PortalProvider>
+        );
+        expect(wrapper.text()).toBe('abc');
+        wrapper.unmount();
+    });
+
+    test('multiple WhitePortal', () => {
+        const wrapper = mount(
+            <PortalProvider>
+                <div>
+                    a
+                    <WhitePortal name="one"/>
+                    <WhitePortal name="one"/>
+                    b
+                    <BlackPortal name="one">1</BlackPortal>
+                    c
+                </div>
+            </PortalProvider>
+        );
+        expect(wrapper.text()).toBe('a11bc');
+        wrapper.unmount();
+    });
+})
+
